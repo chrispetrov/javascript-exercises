5f338435472488a7a22c26cc47e680dc03e04134
Hristijan <chrispetrov904@gmail.com>
2023-03-15 15:44:59 +0100

    finished the last one

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    
+    const oldest=people.reduce((old,item)=>{
+        const oldAge=getAge(old.yearOfBirth,old.yearOfDeath);
+        const curAge=getAge(item.yearOfBirth,item.yearOfDeath);
+        return oldAge<curAge ? item:old;
+    });
+    return oldest;
 };
 
+function getAge(personB,personD){
+    if (!personD){
+        personD=new Date().getFullYear();
+    }
+    return personD-personB;
+
+}
 // Do not edit below this line
 module.exports = findTheOldest;
+
